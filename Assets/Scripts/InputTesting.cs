f24595c500fe78304b3ebfc09ee63cbd6bb0a9e1
TimTheGr8 <tjanssen4@gmail.com>
2023-07-22 18:27:11 -0500

    Tested out rapid prototyping.

--- Assets/Scripts/InputTesting.cs
+++ Assets/Scripts/InputTesting.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputTesting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            Debug.Log("Space key pressed");
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+            Debug.Log("Mouse pressed");
     }
 }
