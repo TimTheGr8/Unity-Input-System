088ca0f4f2d00e8b4c678f3a9f693f0222ac260a
TimTheGr8 <tjanssen4@gmail.com>
2023-07-21 22:03:30 -0500

    Finished registering the Dog actions.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,22 +16,42 @@ public class Player : MonoBehaviour
         //Register perform functions
         _input.Dog.Bark.performed += Bark_performed;
         _input.Dog.Bark.canceled += Bark_canceled;
-
+        // Walk
         _input.Dog.Walk.performed += Walk_performed;
+        // Run
+        _input.Dog.Run.performed += Run_performed;
+        _input.Dog.Run.canceled += Run_canceled;
+        // Die
+        _input.Dog.Die.performed += Die_performed;
+    }
+
+    private void Run_canceled(InputAction.CallbackContext context)
+    {
+        Debug.Log("Runnign done: " + context);
+    }
+
+    private void Die_performed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Die: " + context);
+    }
+
+    private void Run_performed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Running: " + context);
     }
 
     private void Walk_performed(InputAction.CallbackContext context)
     {
-        Debug.Log(context);
+        Debug.Log("Walking: " + context);
     }
 
     private void Bark_canceled(InputAction.CallbackContext context)
     {
-        Debug.Log("Done Barking");
+        Debug.Log("Done Barking " + context);
     }
 
     private void Bark_performed(InputAction.CallbackContext context)
     {
-        Debug.Log("BARK BARK");
+        Debug.Log("BARK BARK" + context);
     }
 }
